a3e247ce6e007f3994402d1a0f06b00c237bf202
avelar <fortunato.avelar@gmail.com>
2019-11-29 09:27:20 -0300

    created function for each page

--- modara-da-floresta-composteira/assets/application.js
+++ modara-da-floresta-composteira/assets/application.js
@@ -15,12 +15,34 @@ $('.section-faq--accordion > dt > a').click(function(e) {
   e.preventDefault();
 });
 
+
+// Select Composteira
+$('input[name="composteira"]').click(function(){
+  if($(this).prop("checked") == true){
+    var composteiraSelected = this.value;
+    console.log(composteiraSelected);
+    const callToAction = $(".button-select-composteira");
+    if (composteiraSelected == "composteira_humi") {
+      callToAction.attr("href", "selecione-a-cor-da-composteira.html");
+    } else if (composteiraSelected == "composteira_morada") {
+      callToAction.attr("href", "selecione-acessorios-composteira-morada.html");
+    }
+  }
+  else if($(this).prop("checked") == false){
+     alert("Selecione uma composteira.");
+  }
+});
+
+
 // Select color
 $('input[id="grey"]').prop('checked', true);
 $('input[name="color"]').click(function(){
   if($(this).prop("checked") == true){
     var colorSelected = this.value;
+    const callToAction = $(".button-select-color");
+    console.log(colorSelected);
     $(".image-color").attr('src', 'assets/images/colors/' + colorSelected + '.jpg');
+    callToAction.attr("href", "selecione-acessorios-composteira-humi-" + colorSelected + ".html")
   }
   else if($(this).prop("checked") == false){
      alert("Ops, selecione uma cor");
@@ -31,8 +53,8 @@ $('input[name="color"]').click(function(){
 $('input[id="none"]').prop('checked', true);
 $('input[name="kit"]').click(function(){
   if($(this).prop("checked") == true){
-    var colorSelected = this.value;
-    console.log(colorSelected);
+    var kitSelected = this.value;
+    console.log(kitSelected);
     // $(".image-color").attr('src', 'assets/images/colors/' + colorSelected + '.jpg');
   }
   else if($(this).prop("checked") == false){
