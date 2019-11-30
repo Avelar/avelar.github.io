b0e5c128bccabf599cd24f025ad62c9ab2196e17
avelar <fortunato.avelar@gmail.com>
2019-11-29 21:46:51 -0300

    text improves

--- modara-da-floresta-composteira/assets/countdownclock.js
+++ modara-da-floresta-composteira/assets/countdownclock.js
@@ -1,30 +1,72 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2020 15:37:25").getTime();
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+function initializeClock(id, endtime) {
+  var clock = document.getElementById("#clockdiv");
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
 
-  // Get today's date and time
-  var now = new Date().getTime();
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="countdownclock"
-  document.getElementById("countdownclockDays").innerHTML = days;
-  document.getElementById("countdownclockHours").innerHTML = hours;
-  document.getElementById("countdownclockMinutes").innerHTML = minutes;
-  document.getElementById("countdownclockSeconds").innerHTML = seconds;
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdownclock").innerHTML = "EXPIRED";
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
   }
-}, 1000);
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
+
+// // Set the date we're counting down to
+// var countDownDate = new Date("Dec 3, 2019 15:37:25").getTime();
+//
+// // Update the count down every 1 second
+// var x = setInterval(function() {
+//
+//   // Get today's date and time
+//   var now = new Date().getTime();
+//
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
+//
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//
+//   // Display the result in the element with id="countdownclock"
+//   document.getElementById("countdownclockDays").innerHTML = days;
+//   document.getElementById("countdownclockHours").innerHTML = hours;
+//   document.getElementById("countdownclockMinutes").innerHTML = minutes;
+//   document.getElementById("countdownclockSeconds").innerHTML = seconds;
+//
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("countdownclock").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
